bef3fe13e23d8b2a41b67d160a80006ccd8dc9d7
Rashad Kazimov <121894918+rashad-kazim@users.noreply.github.com>
2024-01-29 06:26:22 +0100

    s

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -45,7 +45,9 @@ const Navbar = () => {
           hamburger === true ? "right_container right_resp" : "right_container"
         }>
         <nav className="nav_container">
-          <Link to="banner" className="nav_item">
+          <Link
+            to="https://www.youtube.com/watch?v=7wzuievFjrk&ab_channel=CodeWithAjesh"
+            className="nav_item">
             Home
           </Link>
           <Link to="portfolio" className="nav_item">
